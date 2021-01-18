6d74a1eb337fdc226012bb5e57bfd10a49b7e545
apekhnik <iam3ji@gmail.com>
2021-01-18 19:28:21 +0300

    upload to firebase 1st test

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { db, storage } from "../../firebase";
+import firebase from "firebase";
 import {
   selectCameraImage,
   resetCameraImage,
 } from "../../features/cameraSlice";
 import "./index.css";
+import { v4 as uuid } from "uuid";
 import CloseIcon from "@material-ui/icons/Close";
 import TextFieldsIcon from "@material-ui/icons/TextFields";
 import CreateIcon from "@material-ui/icons/Create";
@@ -22,8 +25,39 @@ const Preview = () => {
   const closePreview = () => {
     dispatch(resetCameraImage());
   };
-  const sendPost = () => {};
-  useEffect(() => {}, [history, imgSrc]);
+  const sendPost = () => {
+    const id = uuid();
+    const uploadTask = storage
+      .ref(`./posts/${id}`)
+      .putString(imgSrc, "data-url");
+    uploadTask.on(
+      "state_changed",
+      null,
+      (err) => {
+        //error
+        console.log(err);
+      },
+      () => {
+        //complete
+        storage
+          .ref("posts")
+          .child(id)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              imageUrl: url,
+              username: "iam",
+              read: false,
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+            history.replace("/chats");
+          });
+      }
+    );
+  };
+  useEffect(() => {
+    if (!imgSrc) history.replace("/");
+  }, [history, imgSrc]);
   return (
     <div className="preview">
       <CloseIcon onClick={closePreview} className="preview__close" />
